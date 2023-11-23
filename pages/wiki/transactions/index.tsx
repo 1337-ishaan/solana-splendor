ef178405938c456230e896402318b8a015cb0825
RafaBlockDev <rafafuentesrangel@outlook.com>
2023-11-22 20:26:23 -0600

    feat: add text

--- pages/wiki/transactions/index.tsx
+++ pages/wiki/transactions/index.tsx
@@ -37,86 +37,84 @@ const Unveiling: NextPage = () => {
                     />
                     <div className={"w-[70%] py-[50px] mx-auto max-[800px]:w-full max-[800px]:px-[10px]"}>
                         <div id={"whatIsSplendor"} className={"pt-[65px]"}>
-                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Unveiling SDR: A Decentralized Stable Token Revolution Abstract</p>
+                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Enhancing Security in the Splendor Blockchain Ecosystem: The Repelent Mechanism</p>
                             <p className={"mt-[20px] text-[#3B454E] "}>
-                            Welcome to the forefront of decentralized finance, where we introduce SDR—a groundbreaking decentralized stable token that combines cryptographic innovation with financial stability. In this comprehensive whitepaper, we delve into the conceptual foundations, technical intricacies, and transformative potential that SDR brings to the decentralized financial landscape.
+                            In the ever-evolving landscape of blockchain technology, security is paramount. Splendor Blockchain, fortified by its advanced SRC26 smart contract, has implemented a sophisticated security mechanism known as "Repelent." This mechanism serves as a formidable deterrent, actively safeguarding the ecosystem against bad actors attempting to infiltrate by constantly monitoring and repelling transactions linked to blacklisted addresses identified by international agencies.
                             </p>
                         </div>
 
                         <div id={"benefitsAndUseCases"} className={"pt-[65px]"}>
-                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Introduction</p>
+                            <p className={"text-[24px] text-[#3B454E] font-bold"}>SRC26 Smart Contract Architecture</p>
 
                             <p className={"mt-[20px] text-[#3B454E]"}>
-                            The Rise of SDR in Decentralized Finance
-                            In the dynamic world of decentralized finance, SDR emerges as a beacon of change. It goes beyond being just another token, incorporating meticulous design principles and cryptographic brilliance to redefine stability in the decentralized space.
+                            At the core of Splendor's security infrastructure lies the SRC26 smart contract, meticulously designed to facilitate decentralized token swaps. The Repelent mechanism is intricately woven into the fabric of SRC26, acting as an intelligent gatekeeper to fortify the ecosystem against unauthorized access.
                             </p>
                         </div>
 
                         <div id={"developerGuide"} className={"pt-[65px]"}>
-                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Understanding the SDR Concept</p>
+                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Constant Blacklist Monitoring</p>
 
                             <p className={"mt-[20px] text-[#3B454E]"}>
-                            Drawing Inspiration from IMF's Special Drawing Rights
-                            SDR, or Special Drawing Rights, is at the heart of SDR's design. This decentralized stable token is intricately backed by Splendor (SPL) and draws inspiration from the International Monetary Fund's (IMF) concept of freely usable currencies.
+                            The Repelent mechanism operates by perpetually monitoring blacklisted addresses maintained by recognized international agencies. These addresses are meticulously curated to encompass entities involved in illicit activities, fraud, or any other activities deemed detrimental to the integrity of the blockchain.
                             </p>
                         </div>
 
                         <div id={"developerGuide"} className={"pt-[65px]"}>
-                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Liberating Stability from Singular Fiat Dependencies</p>
+                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Dynamic Link Analysis</p>
 
                             <p className={"mt-[20px] text-[#3B454E]"}>
-                            Unlike stable tokens tied to a single fiat currency, SDR embraces decentralization and mirrors the IMF's SDR pricing model. This liberation from dependency on any single national currency sets SDR apart, positioning it as a stable alternative.
+                            The Repelent mechanism engages in dynamic link analysis, scrutinizing the origin and destination of transactions attempting to enter the Splendor Blockchain. This analysis involves evaluating the transaction history, associated metadata, and any conceivable links to blacklisted addresses
                             </p>
                         </div>
 
                         <div id={"developerGuide"} className={"pt-[65px]"}>
-                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Ethos of Decentralization and Anonymity</p>
+                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Real-Time Transaction Blocking</p>
 
                             <p className={"mt-[20px] text-[#3B454E]"}>
-                            SDR's departure from centralized reserves is a testament to its ethos. Fully backed by SPL held in a permissionless reserve pool, SDR ensures cheaper transactions, faster settlements, and complete anonymity within a truly decentralized financial ecosystem.
+                            Upon identifying a transaction associated with a blacklisted address, the Repelent mechanism swiftly intervenes. Real-time blocking mechanisms are initiated, preventing the unauthorized transfer of tokens from any external blockchain to Splendor. This proactive measure ensures that the ecosystem remains resilient against potential threats.
                             </p>
                         </div>
 
                         <div id={"developerGuide"} className={"pt-[65px]"}>
-                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Stewards of SDR: The Miners</p>
+                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Transparent Governance Protocol</p>
 
-                            <p className={"mt-[20px] text-[#3B454E]"}>Within the Splendor Blockchain, miners play a crucial role as stewards of SDR, earning rewards based on their computational contributions. Those seeking to acquire SDR without mining can participate by locking up SPL as collateral through a smart contract mechanism.</p>
+                            <p className={"mt-[20px] text-[#3B454E]"}>The Repelent mechanism operates under a transparent governance protocol, allowing for continuous updates to the blacklist database based on recommendations from international agencies and the consensus of the Splendor community. This dynamic approach ensures that the system adapts to evolving security threats in real-time.</p>
                         </div>
 
                         <div id={"developerGuide"} className={"pt-[65px]"}>
-                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Ensuring Global Stability through Diversity</p>
+                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Decentralized Decision-Making</p>
 
-                            <p className={"mt-[20px] text-[#3B454E]"}>Pegged to the IMF's SDR and backed by SPL, SDR offers users a diversified portfolio representing a basket of major international currencies. This shields users from the volatility of single currencies, providing a stable alternative in the ever-changing financial landscape.</p>
+                            <p className={"mt-[20px] text-[#3B454E]"}>To fortify decentralization principles, decisions regarding the Repelent mechanism's operation and blacklist updates are subject to decentralized governance. This ensures that the Splendor community actively participates in shaping the security protocols, fostering a collaborative approach to maintaining the integrity of the ecosystem.</p>
                         </div>
 
                         <div id={"developerGuide"} className={"pt-[65px]"}>
-                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Mitigating Economic Uncertainties</p>
+                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Enhanced Security Audits</p>
 
-                            <p className={"mt-[20px] text-[#3B454E]"}>SDR serves as a robust shield against economic uncertainties, offering businesses and individuals engaged in international transactions a reliable hedge against currency risks. This stability enhances confidence and facilitates smoother financial operations.</p>
+                            <p className={"mt-[20px] text-[#3B454E]"}>Splendor Blockchain places a premium on security audits, conducting regular assessments to fortify the Repelent mechanism. External security experts and auditing firms are engaged to rigorously evaluate the SRC26 smart contract, ensuring that it remains impervious to vulnerabilities and emerging threat vectors.</p>
                         </div>
 
                         <div id={"developerGuide"} className={"pt-[65px]"}>
-                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Leveraging the Splendor Blockchain</p>
+                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Preserving Ecosystem Integrity</p>
 
-                            <p className={"mt-[20px] text-[#3B454E]"}>SDR harnesses the capabilities of the Splendor blockchain to ensure transparency and decentralization. The reserve pool smart contract, with autonomous adjustments, creates a trustless and secure financial system that underpins the SDR ecosystem.</p>
+                            <p className={"mt-[20px] text-[#3B454E]"}>Repelent stands as the first line of defense, actively preserving the integrity of the Splendor Blockchain ecosystem. By preventing the ingress of transactions linked to blacklisted addresses, Splendor ensures that the network remains a secure and trustworthy environment.</p>
                         </div>
 
                         <div id={"developerGuide"} className={"pt-[65px]"}>
-                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Streamlining Cross-Border Transactions</p>
+                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Mitigating Risks of Illicit Activities</p>
 
-                            <p className={"mt-[20px] text-[#3B454E]"}>SDR transcends geographical borders, enabling seamless cross-border transactions without the need for currency conversions. This feature positions SDR as a catalyst for international trade and financial efficiency.</p>
+                            <p className={"mt-[20px] text-[#3B454E]"}>The dynamic link analysis and real-time blocking capabilities of Repelent mitigate the risks associated with illicit activities, fraud, and unauthorized access. This not only protects the network but also fosters a secure environment for users engaging in decentralized transactions.</p>
                         </div>
 
                         <div id={"developerGuide"} className={"pt-[65px]"}>
-                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Beyond a Financial Instrument</p>
+                            <p className={"text-[24px] text-[#3B454E] font-bold"}>Upholding Regulatory Compliance</p>
 
-                            <p className={"mt-[20px] text-[#3B454E]"}>Unraveling the layers of SDR reveals more than a financial instrument—it represents a paradigm shift. SDR's vision of financial freedom positions it as a true alternative to Central Bank Digital Currencies, empowering individuals and embodying decentralized principles.</p>
+                            <p className={"mt-[20px] text-[#3B454E]"}>Splendor Blockchain is committed to upholding regulatory compliance. The Repelent mechanism aligns with international standards by actively adhering to blacklists curated by recognized agencies, ensuring that the network operates within legal frameworks.</p>
                         </div>
 
                         <div id={"developerGuide"} className={"pt-[65px]"}>
                             <p className={"text-[24px] text-[#3B454E] font-bold"}>Conclusion</p>
 
-                            <p className={"mt-[20px] text-[#3B454E]"}>In conclusion, SDR stands as a testament to stability, reliability, and inclusivity. As the financial landscape evolves, SDR's decentralized future offers a compelling vision. Embracing the decentralization revolution, SDR emerges as a guiding force, illuminating the path towards a more inclusive and equitable financial ecosystem.</p>
+                            <p className={"mt-[20px] text-[#3B454E]"}>In the realm of blockchain security, the Repelent mechanism implemented within the SRC26 smart contract emerges as a sophisticated and effective deterrent against bad actors attempting to infiltrate the Splendor Blockchain ecosystem. Through constant monitoring, dynamic link analysis, and real-time transaction blocking, Splendor not only fortifies its network against potential threats but also exemplifies a commitment to transparency, decentralization, and the highest standards of security within the decentralized finance landscape.</p>
                         </div>
                     </div>
                 </div>
