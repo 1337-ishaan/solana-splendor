ff0965ca644c8444887a5c56ef473f55a4a0135e
RafaBlockDev <rafafuentesrangel@outlook.com>
2023-11-30 16:08:10 -0600

    feat: add keys

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCGgzGy5crxCkkfK3CIBFQLNHHRuXjkSY",
-  authDomain: "splendor-93ba3.firebaseapp.com",
-  databaseURL: "https://splendor-93ba3-default-rtdb.firebaseio.com",
-  projectId: "splendor-93ba3",
-  storageBucket: "splendor-93ba3.appspot.com",
-  messagingSenderId: "884356411642",
-  appId: "1:884356411642:web:ae8d4e850c942e7d6cd943",
-  measurementId: "G-FV0S0BNWYQ"
+    apiKey: "AIzaSyBd8HYbv_ryAC_lUC9ym0jR001Nh-tuqqA",
+    authDomain: "splendor-490fe.firebaseapp.com",
+    databaseURL: "https://splendor-490fe-default-rtdb.firebaseio.com",
+    projectId: "splendor-490fe",
+    storageBucket: "splendor-490fe.appspot.com",
+    messagingSenderId: "625677282425",
+    appId: "1:625677282425:web:fc1e0e86bdda01aafb78a8",
+    measurementId: "G-H21P6X7XBZ"
 };
 
 const app = initializeApp(firebaseConfig);
