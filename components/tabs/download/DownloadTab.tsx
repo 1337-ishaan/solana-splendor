268f5b409e40b876e735c4a58ce7d83730122587
Jorgecuenca <jorge.cuenca@unillanos.edu.co>
2023-11-30 03:16:36 -0500

    feature/update-dashboard

--- components/tabs/download/DownloadTab.tsx
+++ components/tabs/download/DownloadTab.tsx
@@ -7,7 +7,7 @@ import ubuntuSvg from "../../../assets/icons/download/ubuntu.svg";
 const Download = () => {
     return(
         <div className={"flex flex-col w-full h-full px-[6px] pb-[6px]"}>
-            <p className="mt-[40px] text-center text-[22px] font-bold text-white pt-[0px] pb-[2px]">Download Dashboard</p>
+            <p className="mt-[40px] text-center text-[22px] font-bold text-white pt-[0px] pb-[2px]"></p>
             <p className="mt-[30px] text-center text-[18px] font-thin text-white pt-[0px] pb-[2px] mx-[15%]">
                 Download splendor core and start mining.
             </p>
@@ -15,7 +15,7 @@ const Download = () => {
             <div className="mt-[80px] flex flex-row flex-1 justify-between max-[800px]:flex-col">
                 <div className={"flex flex-col flex-2 pl-[50px] pr-[30px]"}>
                     <p className={"text-left text-white text-[30px] max-[800px]:text-center"}>Splendor Core</p>
-                    <p className={"text-left text-white font-thin mt-[30px] max-[800px]:text-center"}>Download the app to get started with the Splendor Core.</p>
+                    <p className={"text-left text-white font-thin mt-[30px] max-[800px]:text-center"}></p>
                     <div className={"mt-[30px] flex flex-row justify-between"}>
                         <button className={"bg-[#4f49f0] w-[200px] h-[40px] rounded-[5px] text-white cursor-pointer mr-[5px]"}>
                             <div className={"flex flex-row justify-center items-center"}>
