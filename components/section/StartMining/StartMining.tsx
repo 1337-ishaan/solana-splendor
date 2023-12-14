280477d0a0152796de29d4f72494800238542ffd
gabriel <gabozxc@gmail.com>
2023-12-14 19:01:10 -0400

    links to donwload app and new image

--- components/section/StartMining/StartMining.tsx
+++ components/section/StartMining/StartMining.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {NextPage} from "next";
 import Dashboard from "../../../assets/icons/dashboard/dashboard.png";
-import Node from "../../../assets/icons/dashboard/node.png";
+import Node from "../../../assets/icons/dashboard/node.jpg";
 import Image from "next/image";
 import Link from "next/link";
 
--- components/tabs/download/DownloadTab.tsx
+++ components/tabs/download/DownloadTab.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+
 import software_app from "../../../assets/icons/download/Splendor_Core_interface.png";
 import windowsSvg from "../../../assets/icons/download/Group 205.svg";
 import macM1Svg from "../../../assets/icons/download/Group 206.svg";
@@ -11,6 +12,7 @@ const Download = () => {
             id: 1,
             title: "Windows",
             icon: windowsSvg,
+            urlDownload: "",
         },
         {
             id: 4,
@@ -21,14 +23,16 @@ const Download = () => {
             id: 2,
             title: "Apple Silicon",
             icon: macM1Svg,
+            urlDownload: "https://www.dropbox.com/scl/fi/7ft1ckfe04bps5z0bue89/blockchain-downloader-1.0.0-arm64.dmg?rlkey=1pd129unjit0uposvac3vc5cg&dl=1"
         },
         {
             id: 3,
             title: "Apple Intel",
             icon: macIntelSvg,
+            urlDownload: "https://www.dropbox.com/scl/fi/7ft1ckfe04bps5z0bue89/blockchain-downloader-1.0.0-arm64.dmg?rlkey=1pd129unjit0uposvac3vc5cg&dl=1"
         },
     ]
-    return(
+    return (
         <div className={"flex flex-col w-full h-full px-[6px] pb-[6px]"}>
             <div className={"flex flex-3 mt-[50px] mx-auto w-[700px] max-[1000px]:w-full max-[1000px]:px-[10px]"}>
                 <Image src={software_app} alt={"software_app"} />
@@ -40,11 +44,17 @@ const Download = () => {
                 {elements.map((item) => (
                     <div key={item.id} className={"flex flex-col items-center justify-center w-[120px]"}>
                         <div className={"max-[800px]:h-[30px] flex"}>
-                            <Image height={50} src={item.icon} alt={item.title} />
+                            {item.urlDownload ? (
+                                <a href={item.urlDownload} download className="cursor-pointer">
+                                    <Image height={50} src={item.icon} alt={item.title} />
+                                </a>
+                            ) : (
+                                <Image height={50} src={item.icon} alt={item.title} />
+                            )}
                         </div>
                         <p className={"text-white text-[16px] font-thin mt-[10px]"}>{item.title}</p>
                     </div>
-                    ))}
+                ))}
             </div>
         </div>
     )
